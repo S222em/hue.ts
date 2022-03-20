27153cf520b07a11b6684b1b3baea700a5c902a7
S222em <sem.roeten@gmail.com>
2022-03-20 13:18:56 +0100

    feat: Add hue bridge certificate verification
    Signed-off-by: S222em <sem.roeten@gmail.com>

--- src/bridge/rest/Rest.ts
+++ src/bridge/rest/Rest.ts
@@ -4,6 +4,22 @@ import { RouteRateLimit } from './RouteRateLimit';
 import { Routes } from '../../util/Routes';
 import { Agent } from 'https';
 
+const CA =
+	'-----BEGIN CERTIFICATE-----\n' +
+	'MIICMjCCAdigAwIBAgIUO7FSLbaxikuXAljzVaurLXWmFw4wCgYIKoZIzj0EAwIw\n' +
+	'OTELMAkGA1UEBhMCTkwxFDASBgNVBAoMC1BoaWxpcHMgSHVlMRQwEgYDVQQDDAty\n' +
+	'b290LWJyaWRnZTAiGA8yMDE3MDEwMTAwMDAwMFoYDzIwMzgwMTE5MDMxNDA3WjA5\n' +
+	'MQswCQYDVQQGEwJOTDEUMBIGA1UECgwLUGhpbGlwcyBIdWUxFDASBgNVBAMMC3Jv\n' +
+	'b3QtYnJpZGdlMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEjNw2tx2AplOf9x86\n' +
+	'aTdvEcL1FU65QDxziKvBpW9XXSIcibAeQiKxegpq8Exbr9v6LBnYbna2VcaK0G22\n' +
+	'jOKkTqOBuTCBtjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNV\n' +
+	'HQ4EFgQUZ2ONTFrDT6o8ItRnKfqWKnHFGmQwdAYDVR0jBG0wa4AUZ2ONTFrDT6o8\n' +
+	'ItRnKfqWKnHFGmShPaQ7MDkxCzAJBgNVBAYTAk5MMRQwEgYDVQQKDAtQaGlsaXBz\n' +
+	'IEh1ZTEUMBIGA1UEAwwLcm9vdC1icmlkZ2WCFDuxUi22sYpLlwJY81Wrqy11phcO\n' +
+	'MAoGCCqGSM49BAMCA0gAMEUCIEBYYEOsa07TH7E5MJnGw557lVkORgit2Rm1h3B2\n' +
+	'sFgDAiEA1Fj/C3AN5psFMjo0//mrQebo0eKd3aWRx+pQY08mk48=\n' +
+	'-----END CERTIFICATE-----';
+
 export class Rest {
 	public handlers = new Collection<string, RouteRateLimit>();
 	public manager: AxiosInstance;
@@ -14,7 +30,10 @@ export class Rest {
 			headers: {
 				'hue-application-key': applicationKey,
 			},
-			httpsAgent: new Agent({ rejectUnauthorized: false }),
+			httpsAgent: new Agent({
+				ca: CA,
+				checkServerIdentity: () => undefined,
+			}),
 		});
 
 		manager.interceptors.request.use(this.handleRequest.bind(this));
